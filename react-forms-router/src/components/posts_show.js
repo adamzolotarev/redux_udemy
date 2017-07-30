6813731641c30ecd7c2eb962da49e1838d6f2a85
Adam Zolotarev <eugenezolo@gmail.com>
2017-07-29 22:25:56 -0400

    Show individual posts

--- react-forms-router/src/components/posts_show.js
+++ react-forms-router/src/components/posts_show.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
@@ -9,7 +9,23 @@ class PostsShow extends Component {
   }
 
   render() {
-    return <div>Posts Show</div>;
+    const { post } = this.props;
+
+    if (!post) return <div>Loading</div>;
+
+    return (
+      <div>
+        <h3>
+          {post.title}
+        </h3>
+        <h6>
+          Categories: {post.categories}
+        </h6>
+        <p>
+          {post.content}
+        </p>
+      </div>
+    );
   }
 }
 
@@ -17,4 +33,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(null, { fetchPosts })(PostsShow);
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
